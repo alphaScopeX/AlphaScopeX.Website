53cf4e98cfb2aeb8314f4faceef3b0323a649719
AshGrey🥕 <ashgrey.huaier@gmail.com>
2025-07-09 20:35:41 +0800

    :sparkles: feat(components): add `DetailsCandleChart` components

---
+++ components/details-candle-chart.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { CandlestickSeries, ColorType, createChart } from "lightweight-charts";
+import { TradingViewCandleData } from "@/types/candle";
+
+interface DetailsCandleChartProps {
+  data: TradingViewCandleData;
+  colorUp?: string;
+  colorDown?: string;
+  borderVisible?: boolean;
+}
+
+export default function DetailsCandleChart({
+  data = [],
+  colorUp = "#26a69a",
+  colorDown = "#ef5350",
+  borderVisible = false,
+}: DetailsCandleChartProps) {
+  const chartContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const chart = createChart(
+      chartContainerRef.current ?? new HTMLDivElement(),
+      {
+        layout: {
+          textColor: "black",
+          background: {
+            type: ColorType.Solid,
+            color: "white",
+          },
+        },
+      }
+    );
+    const candleStickSeries = chart.addSeries(CandlestickSeries, {
+      upColor: colorUp,
+      downColor: colorDown,
+      borderVisible: borderVisible,
+    });
+    candleStickSeries.setData(data);
+    chart.timeScale().fitContent();
+
+    const handleResize = () => {
+      chart.applyOptions({ width: chartContainerRef.current?.clientWidth });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      chart.remove();
+    };
+  }, [data, colorUp, colorDown, borderVisible]);
+
+  return <div ref={chartContainerRef} />;
+}
